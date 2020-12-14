9c5338a162849a240b436a2d614bba563161581b
eddielewis-thinhle <eddie.thinhle@outlook.com>
2020-12-14 17:26:39 -0500

    update: add sass-node@4.14.1 & create HomePage component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-
+import React from "react";
+import HomePage from "./HomePage/HomePage";
 
 function App() {
-  return (
-    <div >
-     <h1>This is my new projekt!</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>This is my new projekt!</h1>
+			<HomePage />
+		</div>
+	);
 }
 
 export default App;
