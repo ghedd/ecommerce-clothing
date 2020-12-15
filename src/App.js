d486a369dc50e2f38f91e5e2d48bc12d037ed22e
eddielewis-thinhle <eddie.thinhle@outlook.com>
2020-12-15 17:43:32 -0500

    feat: add react-router-dom ; create basic routing system for the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+
+const HatsPage = () => (
+	<div>
+		<h1>HATSSSSSSSSSSSSSSSSSSSSS</h1>
+	</div>
+);
+
+const JacketsPage = () => (
+	<div>
+		<h1>JACKETSSSSSS</h1>
+	</div>
+);
 
 function App() {
 	return (
-		<div>
-			<h1>This is my new projekt!</h1>
-			<HomePage />
-		</div>
+		<>
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route path="/shop/hats" component={HatsPage} />
+				<Route path="/shop/jackets" component={JacketsPage} />
+			</Switch>
+		</>
 	);
 }
 
